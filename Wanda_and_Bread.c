93886132eca767cdc122c06cd139dfc37aebcf1d
yogendrasridatta <119730160+yogendrasridatta@users.noreply.github.com>
2022-12-03 15:54:20 +0530

    #include<stdio.h> int main() {     float n,m,k;     scanf("%f%f%f",&n,&m,&k);     if((n/k)<=m)     {         printf("YES");
        }
        else
        {
            printf("NO");
        }
    }

---
+++ Wanda_and_Bread.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float n,m,k;
+    scanf("%f%f%f",&n,&m,&k);
+    if((n/k)<=m)
+    {
+        printf("YES");
+        
+    }
+    else
+    {
+        printf("NO");
+    }
+}
