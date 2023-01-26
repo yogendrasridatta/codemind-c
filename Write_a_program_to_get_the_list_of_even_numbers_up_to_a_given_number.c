a3b06d78dade31c096ed0c8bb30442539a7b6398
yogendrasridatta <119730160+yogendrasridatta@users.noreply.github.com>
2023-01-26 17:47:55 +0530

    #include<stdio.h> int main() {     int l,u,i;     scanf("%d%d",&l,&u);     for(i=l;i<=u;i++)     {         if(i%2==0)         {             printf("%d ",i);         }     } }

---
+++ Write_a_program_to_get_the_list_of_even_numbers_up_to_a_given_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int l,u,i;
+    scanf("%d%d",&l,&u);
+    for(i=l;i<=u;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+        }
+    }
+}
