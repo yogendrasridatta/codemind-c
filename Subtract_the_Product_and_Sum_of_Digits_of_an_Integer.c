46768fa2ecff099776a2a2925a2d40b7b286d7df
yogendrasridatta <119730160+yogendrasridatta@users.noreply.github.com>
2023-05-24 15:10:40 +0530

    #include<stdio.h> int main() {     int r,n,sum=0,product=1;     scanf("%d",&n);     while(n>0)     {         r=n%10;         n=n/10;         sum=sum+r;         product=product*r;     }     printf("%d",product-sum); }

--- Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,r,sum=0,product=1;
+    int r,n,sum=0,product=1;
     scanf("%d",&n);
-    i=n;
-    while(0<i)
+    while(n>0)
     {
-        r=i%10;
-        i=i/10;
+        r=n%10;
+        n=n/10;
         sum=sum+r;
         product=product*r;
     }
-    printf("%d",((product)-(sum)));
+    printf("%d",product-sum);
 }
